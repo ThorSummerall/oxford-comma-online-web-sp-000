faa50f2a1b7ded722b7fcb52b1c09b0e19faa7b8
Thor Summerall <thor.summerall@gmail.com>
2020-11-20 17:26:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,5 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   if array.length == 1
     return array.join(", ")
   elsif array.length == 2
@@ -6,5 +7,14 @@ def oxford_comma(array)
   else array.length > 2
     last = array.pop
     return array.join(", ") + ", and " + last
+=======
+  case array.length
+    when 1
+      array.join(", ")
+    when 2
+      array.join(" and ")
+    when 3
+      array.join()
+>>>>>>> da4f825ec39767cee0c6eb6d752f3cca7c0f6877
   end
 end
